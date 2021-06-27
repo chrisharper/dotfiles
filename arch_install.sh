17265d3d6d08e986402bcccd08e79e7e742d8faf
Chris Harper <charper+git@charper.co.uk>
2021-06-27 12:41:12 +0100

    Update arch install script

--- arch_install.sh
+++ arch_install.sh
@@ -1,54 +1,97 @@
 #!/bin/sh
-
+echo "Install: loadkeys"
 loadkeys uk
-iwctl 
-timedatectl set-ntp true
-parted /dev/sda mklabel gpt mkpart "EFI" fat32 1MiB 512MiB mkpart "root" btrfs 512MiB 100% set 1 esp on
-mkfs.fat -F32 /dev/sda1
-mkfs.btrfs /dev/sda2
 
+echo "Install: timedatectl"
+timedatectl set-ntp true
+
+echo "Install: parted"
+parted /dev/sda mklabel gpt 
+parted /dev/sda mkpart "EFI" fat32 1MiB 512MiB 
+parted /dev/sda mkpart "root" btrfs 512MiB 100% 
+parted /dev/sda set 1 esp on
+
+echo "Install: mkfs"
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs -f /dev/sda2
+
+echo "Install: mounting"
+mount -o compress=zstd,noatime,discard=async /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-mount -o compress=zstd,noatime /dev/sda2 /mnt
 
+echo "Install: reflector"
 reflector --latest 100 --sort rate --protocol https,https \
   --country 'France,Germany,United Kingdom' --save /etc/pacman.d/mirrorlist
 
-pacstrap /mnt base linux linux-firmware iwd grub efibootmgr zram-generator
+echo "Install: pacstrap"
+pacstrap /mnt base linux linux-firmware iwd zram-generator grub efibootmgr sudo intel-ucode
 
+echo "Install: genfstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
+echo "Install: place second install script"
+cat << EOF > /mnt/root/install.sh
 
+echo "Install: enable iwd.service"
 systemctl enable --now iwd.service
 
+echo "Install: enable IWD DHCP"
+	tee /etc/iwd/main.conf << END > /dev/null
+	[General]
+	EnableNetworkConfiguration=true
+	END
+
+echo "Install: enable systemd-resolved.service
+systemctl enable --now systemd-resolved.service
+
+echo "Install: symlink /etc/resolv.conf"
+rm /etc/resolv.conf
+ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+echo "Install: enable timezone"
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc
 
-sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/'
+echo "Install: set locales"
+sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo 'LANG=en_GB.UTF-8' >> /etc/locale.conf
 echo 'KEYMAP=uk' >> /etc/vconsole.conf
+
+echo "Install: set hostname"
 echo 'arch' >> /etc/hostname
 
-tee /etc/systemd/zram-generator.conf <<- EOF > /dev/null
-[zram0]
-max-zram-size = 8192
-EOF
+echo "Install: setup zram-generator"
 
+	tee /etc/systemd/zram-generator.conf <<- END  > /dev/null
+	[zram0]
+	max-zram-size = 8192
+	END
+
+echo "Install: mkinitcpio"
 mkinitcpio -P
+
+echo "Install: grub"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig  -o /boot/grub/grub.cfg
 
+echo "Install: adduser (charper - no password)"
 useradd -m -G wheel charper
-echo 'password' | passwd charper 
+passwd -d charper
 
+echo "Install: permit wheel group access to sudo"
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
-su charper
-cd /home/charper
-sh -c "$(curl -fsLS git.io/chezmoi) -b ~/.local/bin -- init --apply chrisharper"
+EOF
 
+echo "Install: arch-chroot"
+arch-chroot /mnt sh /root/install.sh
 
+echo "Install: cleanup"
 
+rm /mnt/root/install.sh
+
+echo "Install: finished"
 
 
