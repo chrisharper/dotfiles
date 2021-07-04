5cd9a96780955786d42db25518cf9c9789da46b9
Chris Harper <charper+git@charper.co.uk>
2021-07-04 20:27:03 +0100

    Add treesitter for nicer syntax highlighting Update some additional configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -23,9 +23,12 @@ require "paq" {
   "junegunn/gv.vim";                                -- git commit browser
   "christoomey/vim-tmux-navigator";                 -- vim/tmux integration
   "easymotion/vim-easymotion";                      -- quicker vim motions
+  "nvim-treesitter/nvim-treesitter";                -- syntax
 }
-
-opt.cc = '80'                       -- Line 80 ruler
+local indent, width = 2, 80
+opt.colorcolumn = tostring(width)  -- Line 80 ruler
+opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options
+opt.cursorline = true               -- Highlight cursor line
 opt.expandtab = true                -- Use spaces instead of tabs
 opt.hidden = true                   -- Enable background buffers
 opt.ignorecase = true               -- Ignore case
@@ -35,14 +38,15 @@ opt.number = true                   -- Show line numbers
 opt.relativenumber = true           -- Relative line numbers
 opt.scrolloff = 4                   -- Lines of context
 opt.shiftround = true               -- Round indent
-opt.shiftwidth = 2                  -- Size of an indent
+opt.shiftwidth = indent                  -- Size of an indent
 opt.sidescrolloff = 8               -- Columns of context
 opt.smartcase = true                -- Do not ignore case with capitals
 opt.smartindent = true              -- Insert indents automatically
 opt.splitbelow = true               -- Put new windows below current
 opt.splitright = true               -- Put new windows right of current
-opt.tabstop = 2                     -- Number of spaces tabs count for
+opt.tabstop = indent                     -- Number of spaces tabs count for
 opt.termguicolors = true            -- True color support
+opt.textwidth = width               -- Maximum width of text
 opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
 opt.wrap = false                    -- Disable line wrap
 
@@ -60,7 +64,10 @@ g.lightline = {
        },
 }
 
-
+require('nvim-treesitter.configs').setup {
+  ensure_installed = 'maintained',
+  highlight = {enable = true},
+}
 
 -- vim-easymotion
 g.EasyMotion_do_mapping = 0
