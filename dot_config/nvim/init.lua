e3daa909fe26e3e0b94f50c69fe86ec794cb6c95
Chris Harper <charper+git@charper.co.uk>
2021-07-11 20:06:02 +0100

    Split nvim config into file per plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,162 +1,31 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to seDTreeToggle
-local api  = vim.api
-local execute = vim.api.nvim_command
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
-
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-require ('packer').startup(function()
-  use "wbthomason/packer.nvim";                                  -- plugin manager
-  use "gruvbox-community/gruvbox";                                -- colourscheme 
-  use "nvim-treesitter/nvim-treesitter";                -- syntax
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'},{'nvim-telescope/telescope-fzy-native.nvim'}}
-  }
-
-  use {
-    'hrsh7th/nvim-compe',
-    requires = {{'neovim/nvim-lspconfig','kabouzeid/nvim-lspinstall'}}
-  }
-end)
-
 local indent, width = 2, 80
-opt.colorcolumn = tostring(width)  -- Line 80 ruler
-opt.completeopt = {'menuone', 'noselect'}  -- Completion options
-opt.cursorline = true               -- Highlight cursor line
-opt.expandtab = true                -- Use spaces instead of tabs
-opt.hidden = true                   -- Enable background buffers
-opt.ignorecase = true               -- Ignore case
-opt.joinspaces = false              -- No double spaces with join
-opt.list = true                     -- Show some invisible characters
-opt.number = true                   -- Show line numbers
-opt.relativenumber = true           -- Relative line numbers
-opt.scrolloff = 4                   -- Lines of context
-opt.shell= 'bash --login'
-opt.shiftround = true               -- Round indent
-opt.shiftwidth = indent             -- Size of an indent
-opt.sidescrolloff = 8               -- Columns of context
-opt.signcolumn = 'yes'
-opt.smartcase = true                -- Do not ignore case with capitals
-opt.smartindent = true              -- Insert indents automatically
-opt.splitbelow = true               -- Put new windows below current
-opt.splitright = true               -- Put new windows right of current
-opt.tabstop = indent                 -- Number of spaces tabs count for
-opt.termguicolors = true            -- True color support
-opt.textwidth = width               -- Maximum width of text
-opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
-opt.wrap = false                    -- Disable line wrap
+vim.opt.colorcolumn = tostring(width)  -- Line 80 ruler
+vim.opt.completeopt = {'menuone', 'noselect'}  -- Completion options
+vim.opt.cursorline = true               -- Highlight cursor line
+vim.opt.expandtab = true                -- Use spaces instead of tabs
+vim.opt.hidden = true                   -- Enable background buffers
+vim.opt.ignorecase = true               -- Ignore case
+vim.opt.joinspaces = false              -- No double spaces with join
+vim.opt.list = true                     -- Show some invisible characters
+vim.opt.number = true                   -- Show line numbers
+vim.opt.relativenumber = true           -- Relative line numbers
+vim.opt.scrolloff = 4                   -- Lines of context
+vim.opt.shell= 'bash --login'
+vim.opt.shiftround = true               -- Round indent
+vim.opt.shiftwidth = indent             -- Size of an indent
+vim.opt.sidescrolloff = 8               -- Columns of context
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true                -- Do not ignore case with capitals
+vim.opt.smartindent = true              -- Insert indents automatically
+vim.opt.splitbelow = true               -- Put new windows below current
+vim.opt.splitright = true               -- Put new windows right of current
+vim.opt.tabstop = indent                 -- Number of spaces tabs count for
+vim.opt.termguicolors = true            -- True color support
+vim.opt.textwidth = width               -- Maximum width of text
+vim.opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
+vim.opt.wrap = false                    -- Disable line wrap
 
-cmd 'colorscheme gruvbox'
-
-g.mapleader = ' '
-
-require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained',
-  highlight = {enable = true},
-}
+vim.cmd 'colorscheme gruvbox'
+vim.g.mapleader = ' '
 
 
-require('telescope').setup {
-  defaults = {
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    file_sorter = require('telescope.sorters').get_fzy_sorter,
-  },
-  extensions = {
-    fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-    }
-  }
-}
-require('telescope').load_extension('fzy_native')
-
-
-
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
-  end
-end
-
-setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
-
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  resolve_timeout = 800;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-    max_width = 120,
-    min_width = 60,
-    max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  };
-
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = true;
-    ultisnips = true;
-    luasnip = true;
-  };
-}
-
-
-
--- See `:help vim.lsp.*` for documentation on any of the below functions    
-map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>')
-map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
-map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>')
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
-map("n", "<space>F", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-
-
-map('n','<leader>ff',"<cmd>lua require('telescope.builtin').find_files() <cr>")
-map('n','<leader>fg',"<cmd>lua require('telescope.builtin').live_grep() <cr>")
-map('n','<leader>fb',"<cmd>lua require('telescope.builtin').buffers() <cr>")
-map('n','<leader>fG',"<cmd>lua require('telescope.builtin').git_commits() <cr>")
