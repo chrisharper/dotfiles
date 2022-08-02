47bd6719ebe765ac7553d1ddfaf80630044b652d
Chris Harper <charper+git@charper.co.uk>
2022-08-02 18:40:44 +0100

    Monolithic config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,64 @@
+--auto install package manager if not present
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.api.nvim_command.execute('packadd packer.nvim')
+end
+
+require ('packer').startup(function()
+
+  -- plugin manager
+  use "wbthomason/packer.nvim";
+
+  --colorscheme
+  use "gruvbox-community/gruvbox";
+
+  -- syntax
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({ensure_installed='all'})
+    end
+  }
+
+  -- fuzzy finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {'nvim-lua/popup.nvim','nvim-lua/plenary.nvim',
+                'nvim-telescope/telescope-fzy-native.nvim'},
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+          grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+          file_sorter = require('telescope.sorters').get_fzy_sorter,
+        },
+        extensions = {
+          fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+          }
+        }
+      }
+      require('telescope').load_extension('fzy_native')
+    end
+  }
+
+  -- git gutter line
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+end)
+
+
 local indent, width = 2, 80
 vim.opt.colorcolumn = tostring(width)  -- Line 80 ruler
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}  -- Completion options
@@ -28,3 +89,9 @@ vim.opt.wrap = false                    -- Disable line wrap
 vim.cmd 'colorscheme gruvbox'
 vim.g.mapleader = ' '
 
+
+-- telescope
+vim.api.nvim_set_keymap("n", "<leader>ff", "<Cmd>lua require('telescope.builtin').find_files() <CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", "<Cmd>lua require('telescope.builtin').live_grep() <CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", "<Cmd>lua require('telescope.builtin').buffers() <CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fG", "<Cmd>lua require('telescope.builtin').git_commits() <CR>", { noremap = true })
