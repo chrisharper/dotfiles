9c57c043e71893b55437253882361e69684de205
Chris Harper <charper+git@charper.co.uk>
2022-08-02 21:44:57 +0100

    note about rust-analyzer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
+-- ln -s ~/.rustup/toolchains/nightly//bin/rust-analyzer
+-- ~/.cargo/bin/rust-analyzer 
+
+
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
