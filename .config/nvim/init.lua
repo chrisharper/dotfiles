f1bf929caf045b7d76c11ca5337296cf2f6e7650
Chris Harper <charper+git@charper.co.uk>
2023-03-03 22:18:05 +0000

    NVIM rehash

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,45 +1,47 @@
 -- ln -s ~/.rustup/toolchains/nightly//bin/rust-analyzer
 -- ~/.cargo/bin/rust-analyzer
-
-
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
-  vim.api.nvim_command.execute('packadd packer.nvim')
-end
-
-
 local indent, width = 2, 80
-vim.opt.colorcolumn = tostring(width) -- Line 80 ruler
+vim.opt.colorcolumn = tostring(width)                   -- Line 80 ruler
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' } -- Completion options
-vim.opt.cursorline = true -- Highlight cursor line
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.hidden = true -- Enable background buffers
-vim.opt.ignorecase = true -- Ignore case
-vim.opt.joinspaces = false -- No double spaces with join
-vim.opt.list = true -- Show some invisible characters
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.cursorline = true                               -- Highlight cursor line
+vim.opt.expandtab = true                                -- Use spaces instead of tabs
+vim.opt.hidden = true                                   -- Enable background buffers
+vim.opt.ignorecase = true                               -- Ignore case
+vim.opt.joinspaces = false                              -- No double spaces with join
+vim.opt.list = true                                     -- Show some invisible characters
+vim.opt.number = true                                   -- Show line numbers
+vim.opt.relativenumber = true                           -- Relative line numbers
+vim.opt.scrolloff = 4                                   -- Lines of context
 vim.opt.shell = 'bash --login'
-vim.opt.shiftround = true -- Round indent
-vim.opt.shiftwidth = indent -- Size of an indent
-vim.opt.sidescrolloff = 8 -- Columns of context
+vim.opt.shiftround = true                               -- Round indent
+vim.opt.shiftwidth = indent                             -- Size of an indent
+vim.opt.sidescrolloff = 8                               -- Columns of context
 vim.opt.signcolumn = 'yes'
-vim.opt.smartcase = true -- Do not ignore case with capitals
-vim.opt.smartindent = true -- Insert indents automatically
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.tabstop = indent -- Number of spaces tabs count for
-vim.opt.termguicolors = true -- True color support
-vim.opt.textwidth = width -- Maximum width of text
-vim.opt.wildmode = { 'list', 'longest' } -- Command-line completion mode
-vim.opt.wrap = false -- Disable line wrap
+vim.opt.smartcase = true                                -- Do not ignore case with capitals
+vim.opt.smartindent = true                              -- Insert indents automatically
+vim.opt.splitbelow = true                               -- Put new windows below current
+vim.opt.splitright = true                               -- Put new windows right of current
+vim.opt.tabstop = indent                                -- Number of spaces tabs count for
+vim.opt.termguicolors = true                            -- True color support
+vim.opt.textwidth = width                               -- Maximum width of text
+vim.opt.wildmode = { 'list', 'longest' }                -- Command-line completion mode
+vim.opt.wrap = false                                    -- Disable line wrap
 
-vim.cmd 'colorscheme gruvbox'
 vim.g.mapleader = ' '
 
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd.packadd('packer.nvim')
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function()
   local use = require('packer').use
@@ -47,12 +49,20 @@ require('packer').startup(function()
   use "wbthomason/packer.nvim";
 
   --colorscheme
-  use "gruvbox-community/gruvbox";
+  use {
+    "gruvbox-community/gruvbox",
+    config = function()
+      vim.cmd 'colorscheme gruvbox'
+    end
+  }
 
   -- syntax
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
     config = function()
       require('nvim-treesitter.configs').setup({ ensure_installed = 'all' })
     end
@@ -61,15 +71,10 @@ require('packer').startup(function()
   -- fuzzy finder
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim',
+    requires = { 'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-fzy-native.nvim' },
     config = function()
       require('telescope').setup {
-        defaults = {
-          file_previewer = require 'telescope.previewers'.vim_buffer_cat.new,
-          grep_previewer = require 'telescope.previewers'.vim_buffer_vimgrep.new,
-          file_sorter = require('telescope.sorters').get_fzy_sorter,
-        },
         extensions = {
           fzy_native = {
             override_generic_sorter = false,
@@ -81,80 +86,44 @@ require('packer').startup(function()
     end
   }
 
-  -- nvim-cmp
   use {
-    'hrsh7th/nvim-cmp',
-    requires = { 'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline',
-      'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', "simrat39/rust-tools.nvim" },
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    },
     config = function()
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require 'cmp'
-
-      cmp.setup({
-        snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-
-          end,
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
-        }, {
-          { name = 'buffer' },
-        })
+      local lsp = require('lsp-zero').preset({
+        name = 'recommended',
+        set_lsp_keymaps = true,
+        manage_nvim_cmp = true,
+        suggest_lsp_servers = false,
       })
-
+      lsp.setup()
+      vim.diagnostic.config({
+        virtual_text = true
+      })
     end
   }
 
   -- git gutter line
   use {
     'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
     config = function()
       require('gitsigns').setup()
     end
@@ -175,9 +144,12 @@ require('packer').startup(function()
   }
   -- tmux <-> navigation
   use 'christoomey/vim-tmux-navigator'
+
+  -- packer packer_bootstrap
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
-
-
 
 
 
@@ -189,80 +161,3 @@ vim.api.nvim_set_keymap("n", "<leader>fG", "<Cmd>lua require('telescope.builtin'
 
 -- nvim-tree
 vim.api.nvim_set_keymap("n", "<leader>t", ':NvimTreeToggle <CR>', { noremap = true })
-
--- nvim-lspconfig
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Setup buffer-local keymaps / options for LSP buffers
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local lsp_attach = function(client, buf)
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = buf }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-
-  vim.api.nvim_buf_set_option(buf, "formatexpr", "v:lua.vim.lsp.formatexpr()")
-  vim.api.nvim_buf_set_option(buf, "omnifunc", "v:lua.vim.lsp.omnifunc")
-  vim.api.nvim_buf_set_option(buf, "tagfunc", "v:lua.vim.lsp.tagfunc")
-end
-
--- Setup rust_analyzer via rust-tools.nvim
-require("rust-tools").setup({
-  server = {
-    capabilities = capabilities,
-    on_attach = lsp_attach,
-    settings = {
-      ["rust-analyzer"] = {
-        checkOnSave = {
-          command = "clippy"
-        },
-      }
-    }
-  }
-})
-
-require 'lspconfig'.sumneko_lua.setup {
-  capabilities = capabilities,
-  on_attach = lsp_attach,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
